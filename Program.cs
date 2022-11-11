9ccb786d7d4aae50eb20310f323b99a8154d9881
DanieleArr <daniele.arruda@idexsolucoes.com.br>
2022-11-11 09:56:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SistemaBancario.SistemaBanco;
+
+
+namespace SistemaBancario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta();
+
+            conta.NomeConta = "Conta Teste";
+            conta.NumeroConta = 1000;
+            conta.Agencia = 001;
+            conta.Saldo = 100.55;
+
+            conta.Titular = new Titular()
+            {
+                Cpf = "234456567",
+                Rg = "233452323",
+                Endereco = "Rua Test",
+                Nome = "Joao Santos"
+            };
+
+            Console.WriteLine($"O saldoa atual é: {conta.VerSaldo()}");
+            conta.Depositar(150);
+
+            Console.WriteLine($"O saldoa atual após deposito é: " + conta.VerSaldo());
+
+            conta.Sacar(50);
+            Console.WriteLine("O saldoa atual após saque é: {0}", conta.VerSaldo());
+
+            conta.Sacar(2000);
+        }
+
+
+
+    }
+}
---
+++ SistemaBanco/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBancario.SistemaBanco
+{
+    class Conta
+    {
+        public int Agencia { get; set; }
+
+        public int NumeroConta { get; set; }
+
+        public string NomeConta { get; set; }
+
+        public Titular Titular { get; set; }
+
+        public Double Saldo { get; set; }
+
+        public Double VerSaldo()
+        {
+            return Saldo;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor < Saldo) Saldo -= valor;
+            else Console.WriteLine("Saldo insuficiente");
+        }
+    }
+}
